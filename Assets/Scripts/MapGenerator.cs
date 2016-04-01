84db395f8e9f022889657444b09fe211b89d9eac
DarkGamer67 <utku2000@hotmail.de>
2016-04-01 17:32:35 +0200

    Improved MapGen and added Utility
    Added a way to shuffle an array and added a class Coord

--- Assets/Scripts/MapGenerator.cs
+++ Assets/Scripts/MapGenerator.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class MapGenerator : MonoBehaviour
 {
@@ -7,11 +8,11 @@ public class MapGenerator : MonoBehaviour
     public Transform groundPrefabWhite;
     public Transform groundPrefabBlack;
     public Vector2 mapSize;
+    public Coord startingCoord;
 
     public float tileSize;
 
-    // Use this for initialization
-    void Start ()
+    void Start()
     {
         GenerateMap();
     }
@@ -32,20 +33,53 @@ public class MapGenerator : MonoBehaviour
             for (int y = 0; y < mapSize.y; y++)
             {
                 Transform newTile;
+                Coord tileCoord = new Coord(x, y);
+
                 if ((x + y) % 2 == 1)
                 {
-                    newTile = Instantiate(groundPrefabBlack, new Vector3(x * tileSize, 0, y * tileSize), Quaternion.Euler(Vector3.right * 90)) as Transform;
+                    newTile = Instantiate(groundPrefabBlack, Coord.ToVector3(tileCoord), Quaternion.Euler(Vector3.right * 90)) as Transform;
 
                 }
                 else
                 {
-                    newTile = Instantiate(groundPrefabWhite, new Vector3(x * tileSize, 0, y * tileSize), Quaternion.Euler(Vector3.right * 90)) as Transform;
+                    newTile = Instantiate(groundPrefabWhite, Coord.ToVector3(tileCoord), Quaternion.Euler(Vector3.right * 90)) as Transform;
                 }
                 newTile.parent = mapHolder;
-                newTile.localScale = new Vector3(tileSize, 0, tileSize);
+                newTile.localScale = Vector3.one * tileSize;
             }
         }
+    }
 
+    bool MapIsFullyAccesible(bool[,] obstacleMap, int currentObstacleCount)
+    {
+        int accessibleTileCount = 1;
+        bool[,] mapFlags = new bool[obstacleMap.GetLength(0), obstacleMap.GetLength(1)];
+        Queue<Coord> queue = new Queue<Coord>();
+        queue.Enqueue(startingCoord);
+        mapFlags[startingCoord.x, startingCoord.y] = true;
+
+        while(queue.Count > 0)
+        {
+            Coord currentCoord = queue.Dequeue();
+            for(int x = -2; x <= 2; x++)
+            {
+                for (int y = -2; y <= 2; y++)
+                {
+                    if(!(Mathf.Abs(x) == 2 && Mathf.Abs(y) == 2) || (Mathf.Abs(x) == 2 && y == 0) || (x == 0 && Mathf.Abs(y) == 2) || (x == 0 && y == 0))
+                    {
+                        int neighbourX = currentCoord.x + x;
+                        int neighbourY = currentCoord.y + y;
+                        if ((neighbourX >= 0 && neighbourX < mapSize.x) && (neighbourY >= 0 && neighbourY < mapSize.y))
+                        {
+                            accessibleTileCount++;
+                            queue.Enqueue(new Coord(neighbourX, neighbourY));
+                            mapFlags[neighbourX, neighbourY] = true;
+                        }
+                    }
+                }
+            }
+        }
+        return accessibleTileCount == mapSize.y * mapSize.x - currentObstacleCount;
 
     }
 }
---
+++ Assets/Scripts/Utility.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using UnityEngine;
+
+public static class Utility
+{
+    public static T[] ShuffleArray<T>(T[] arrayToShuffle, int seed)
+    {
+        System.Random random = new System.Random(seed);
+        for(int i = 0; i < arrayToShuffle.Length - 1; i++)
+        {
+            int randomNumber = random.Next(i, arrayToShuffle.Length);
+
+            T tempObject = arrayToShuffle[i];
+            arrayToShuffle[i] = arrayToShuffle[randomNumber];
+            arrayToShuffle[randomNumber] = tempObject;
+        }
+        return arrayToShuffle;
+    }
+    public static T[] ShuffleArray<T>(T[] arrayToShuffle)
+    {
+        System.Random random = new System.Random();
+        for (int i = 0; i < arrayToShuffle.Length - 1; i++)
+        {
+            int randomNumber = random.Next(i, arrayToShuffle.Length);
+
+            T tempObject = arrayToShuffle[i];
+            arrayToShuffle[i] = arrayToShuffle[randomNumber];
+            arrayToShuffle[randomNumber] = tempObject;
+        }
+        return arrayToShuffle;
+    }
+}
+
+[Serializable]
+public class Coord
+{
+    
+    public int x;
+    public int y;
+
+    public Coord(int x, int y)
+    {
+        this.x = x;
+        this.y = y;
+        
+    }
+
+    public static Vector3 ToVector3(Coord coord)
+    {
+        float tileSize = GameObject.FindObjectOfType<MapGenerator>().GetComponent<MapGenerator>().tileSize;
+        return new Vector3(coord.x * tileSize, 0, coord.y * tileSize);
+    }
+
+    public static bool operator ==(Coord c1, Coord c2)
+    {
+        return ((c1.x == c2.x) && (c1.y == c2.y));
+    }
+    public static bool operator !=(Coord c1, Coord c2)
+    {
+        return !(c1 == c2);
+    }
+
+}
